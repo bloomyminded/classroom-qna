e2ddd5ce6ad0640b8b9eb32579beeb4e8d9b53a9
Cody Roberts <codyjroberts@gmail.com>
2015-08-15 13:29:27 -0500

    Added #new, #create, #comment_params to model
    Enables the creation of comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @comment  = @question.comments.create(comment_params)
+
+    if @comment.save
+      redirect_to questions_path, notice: "Comment was created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :question_id)
+  end
+
 end
