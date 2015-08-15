3f1303a1f0082807d6de1af4a0fcffe9435f4959
Cody Roberts <codyjroberts@gmail.com>
2015-08-15 17:14:37 -0500

    Count votes based on existence, depend on destroy
    Votes now are counted purely on existing in the database and not up or
    down votes.  With the decision to remove down votes, up votes no longer
    need to exist.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,13 @@
 class Question < ActiveRecord::Base
-  has_many :comments
-  has_many :votes
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   def get_votes
     count = 0
     votes.each do |v|
-      count += v.up
+      count += 1 
     end
     count
   end
+
 end
