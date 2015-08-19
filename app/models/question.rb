099c9045a83eea2d28a9fa87d6ca6e6f83253e95
Cody Roberts <codyjroberts@gmail.com>
2015-08-18 22:27:08 -0500

    Add temp relations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ActiveRecord::Base
+  belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
-
+  
   def get_votes
     count = 0
     votes.each do |v|
