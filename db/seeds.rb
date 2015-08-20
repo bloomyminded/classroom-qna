ac4ee55ca53bb6ebf1d1e1cf4b4f944f1d3a5895
Cody Roberts <codyjroberts@gmail.com>
2015-08-20 18:41:45 -0500

    New seeds!
    Properly generates users, questions, and comments using faker.  Admin
    also created.
    
    email: admin@qna.com
    password: password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,76 +1,34 @@
+require 'faker'
+
+User.destroy_all
 Question.destroy_all
 Comment.destroy_all
 Vote.destroy_all
 
-Question.create!([
-  {
-    body: "Is it a stack or a queue thats FIFO?" 
-  },
-  {
-    body: "How many questions are on the quiz tomorrow?" 
-  },
-  {
-    body: "What is the running time of radix tree? Why use them?" 
-  },
-  {
-    body: "Can someone repeat what the instructor said about Dijkstra's runtime?" 
-  },
-  {
-    body: "Does anyone have the link to tomorrow's homework assignment?" 
-  },
-  {
-    body: "Arrived a bit late, is the quiz still on for today?" 
-  }
-])
+User.new(name: "Admin", 
+         email: "admin@qna.com", 
+         password: "password", 
+         password_confirmation: "password").save!(validate: false)
 
-questions = Question.all
+99.times do |n|
+  name  = Faker::Name.name
+  email = "#{name}@email.org"
+  password = "password"
+  User.new(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password).save!(validate: false)
+end
 
-Comment.create!([
-  {
-    question_id: questions[0].id,
-    body: "That would be a queue"
-  },
-  {
-    question_id: questions[1].id,
-    body: "I heard 30"
-  },
-  {
-    question_id: questions[1].id,
-    body: "30 is correct. 20 multiple choice 10 short answer"
-  },
-  {
-    question_id: questions[2].id,
-    body: "Lookup, insertion, and deletion in O(n).  Even though radix trees aren't in the class of O(log n), they often have less comparisons and less nodes than balanced trees"
-  }
-])
+20.times do |n|
+  body = Faker::Lorem.paragraph
+  userid = 1..99
+  Question.new(body: body, user_id: userid).save!
+end
 
-Vote.create!([
-  {
-    question_id: questions[3].id,
-    up: 1
-  },
-  {
-    question_id: questions[3].id,
-    up: 1
-  },
-  {
-    question_id: questions[3].id,
-    up: 1
-  },
-  {
-    question_id: questions[3].id,
-    up: 1
-  },
-  {
-    question_id: questions[2].id,
-    up: 1
-  },
-  {
-    question_id: questions[1].id,
-    up: 1
-  },
-  {
-    question_id: questions[0].id,
-    up: 1
-  }
-])
+32.times do |n|
+  body = Faker::Lorem.sentence
+  questionid = rand(1..20)
+  User.limit(1).order("RANDOM()").first.comments.create(body: body,
+                                                      question_id: questionid)
+end
