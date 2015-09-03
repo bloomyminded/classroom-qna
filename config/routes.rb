81465efa1f05fd5bcd0a069503edc790d4b430f4
Cody Roberts <codyjroberts@gmail.com>
2015-09-03 16:39:21 -0500

    Add first and last name functionality to edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    omniauth: 'users/omniauth',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks'
+  }
 
   root "home#index"
 end
