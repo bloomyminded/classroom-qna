f978e0f5573560c67af527675a2ab78ee1133db1
Cody Roberts <codyjroberts@gmail.com>
2015-09-04 16:09:48 -0500

    New seed includes admin, user, school, course

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+School.destroy_all
+Course.destroy_all
+Lecture.destroy_all
+User.destroy_all
+Question.destroy_all
+Comment.destroy_all
+Vote.destroy_all
+
+User.new(first_name: "Admin", 
+         last_name: "Flapjack",
+         email: "admin@qna.com", 
+         password: "password", 
+         password_confirmation: "password").save!(validate: false)
+
+50.times do |n|
+  first_name  = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = Faker::Internet.safe_email(first_name + last_name) 
+  password = "password"
+  User.new(first_name: first_name,
+           last_name: last_name,
+           email: email,
+           password:              password,
+           password_confirmation: password).save!(validate: false)
+end
+
+10.times do
+  name = (0...3).map { (65 + rand(26)).chr  }.join 
+  city = Faker::Address.city
+  state = Faker::Address.state_abbr
+  zip = Faker::Address.zip
+  School.new(name: name,
+             city: city,
+             state: state,
+             zip: zip).save!
+end
+
+10.times do |s|
+  4.times do |c|
+    name = (0...2).map { (65 + rand(26)).chr }.join + " 3#{s}#{c}"
+    description = Faker::Lorem.paragraph
+    School.where(id: s+1).first.courses.new(name: name,
+                                            description: description).save!
+  end
+end
+
+#20.times do |n|
+#  body = Faker::Lorem.paragraph
+#  User.limit(1).order("RANDOM()").first.questions.create(body: body)
+#end
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#32.times do |n|
+#  body = Faker::Lorem.sentence
+#  questionid = rand(1..20)
+#  User.limit(1).order("RANDOM()").first.comments.create(body: body,
+#                                                      question_id: questionid)
+#end
