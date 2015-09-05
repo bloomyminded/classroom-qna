40be771d1232940beccdaa1b69dfaccb57360f98
Cody Roberts <codyjroberts@gmail.com>
2015-09-05 13:13:25 -0500

    Add admin/student auth and update question views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :get_question
+  before_action :authenticate_student!
 
   def index
     @questions = Question.all.order(created_at: :desc)
@@ -15,6 +17,26 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def authenticate_student!
+    if ( current_user.is_admin? || current_user.is_student?(@course) )
+      true
+    else
+      return redirect_to "/"
+    end
+  end
+
+  def get_question
+    @question = Question.find(params[:id])
+    @lecture = @question.lecture
+    @course = @lecture.course
+  end
+
+  def private_params
+    params.require(:question)
   end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,8 @@ class Question < ActiveRecord::Base
   belongs_to :lecture
   has_many :comments
   has_many :votes
+
+  def get_votes 
+    self.votes.count
+  end
 end
