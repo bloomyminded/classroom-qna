d7ada3e3b7a95c0eb3b42d6c6161c537f6f1b210
Cody Roberts <codyjroberts@gmail.com>
2015-09-05 11:16:24 -0500

    New seeding for relations

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,10 @@ User.new(first_name: "Admin",
          last_name: "Flapjack",
          email: "admin@qna.com", 
          password: "password", 
-         password_confirmation: "password").save!(validate: false)
+         password_confirmation: "password",
+         is_admin: true).save!(validate: false)
 
-50.times do |n|
+300.times do |n|
   first_name  = Faker::Name.first_name
   last_name = Faker::Name.last_name
   email = Faker::Internet.safe_email(first_name + last_name) 
@@ -24,6 +25,7 @@ User.new(first_name: "Admin",
            password_confirmation: password).save!(validate: false)
 end
 
+#Schools
 10.times do
   name = (0...3).map { (65 + rand(26)).chr  }.join 
   city = Faker::Address.city
@@ -35,23 +37,45 @@ end
              zip: zip).save!
 end
 
-10.times do |s|
+#Courses
+School.all.each do |s|
   4.times do |c|
-    name = (0...2).map { (65 + rand(26)).chr }.join + " 3#{s}#{c}"
+    name = "CS 10#{c}"
     description = Faker::Lorem.paragraph
-    School.where(id: s+1).first.courses.new(name: name,
-                                            description: description).save!
+    course = s.courses.create(name: name, description: description)
+
+    30.times do
+      student = User.all.limit(1).order("RANDOM()").first
+      course.students << student
+    end
   end
 end
 
-#20.times do |n|
-#  body = Faker::Lorem.paragraph
-#  User.limit(1).order("RANDOM()").first.questions.create(body: body)
-#end
-#
-#32.times do |n|
-#  body = Faker::Lorem.sentence
-#  questionid = rand(1..20)
-#  User.limit(1).order("RANDOM()").first.comments.create(body: body,
-#                                                      question_id: questionid)
-#end
+#Lectures
+Course.all.each do |c|
+  5.times do
+    title = Faker::Lorem.sentence(3).capitalize
+    c.lectures.create(title: title)
+  end
+end
+
+#Questions
+Lecture.all.each do |l|
+  students = l.course.students
+  10.times do
+    student = students.limit(1).order("RANDOM()").first
+    body = Faker::Lorem.paragraph
+    student.questions.create(body: body, lecture_id: l.id)
+  end
+end
+
+#Comments
+Question.all.each do |q|
+  students = q.lecture.course.students
+  body = Faker::Lorem.sentence
+  questionid = q.id
+  3.times do |c|
+    student = students.limit(1).order("RANDOM()").first
+    student.comments.create(body: body, question_id: questionid)
+  end
+end
